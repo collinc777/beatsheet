dce238a962245bdebf2c315a6684d2299301adfa
Collin Caram <collin@nubrakes.com>
2024-01-12 23:02:32 -0600

    i believe it's functionally comlete

--- app/beatsheet/BeatsheetView.tsx
+++ app/beatsheet/BeatsheetView.tsx
@@ -140,9 +140,23 @@ function AddAct({
   position: number;
   addActAtPosition: any;
 }) {
+  const [showForm, setShowForm] = React.useState(false);
   const [title, setTitle] = React.useState("");
+  if (!showForm) {
+    return (
+      <div className="flex flex-col justify-center p-3">
+        <Button
+          onClick={() => {
+            setShowForm(true);
+          }}
+        >
+          Add Act
+        </Button>
+      </div>
+    );
+  }
   return (
-    <div>
+    <div className={"flex space-x-3 py-3"}>
       <Input
         type="text"
         onChange={(e) => {
@@ -193,7 +207,7 @@ function AddBeat({
   }
 
   return (
-    <div>
+    <div className="px-4">
       <Input
         type="text"
         autoFocus
@@ -202,7 +216,7 @@ function AddBeat({
         onChange={(e) => setDescription(e.target.value)}
       />
       <Input
-        type="text"
+        type="number"
         placeholder="Duration"
         value={duration}
         onChange={(e) => setDuration(e.target.value)}
@@ -213,23 +227,33 @@ function AddBeat({
         value={cameraAngle}
         onChange={(e) => setCameraAngle(e.target.value)}
       />
-      <Button
-        variant={"secondary"}
-        onClick={() => {
-          addBeat(
-            {
-              description: description,
-              durationSeconds: parseInt(duration),
-              cameraAngle: cameraAngle,
-              id: nanoid(),
-            },
-            actId,
-            position
-          );
-        }}
-      >
-        Add
-      </Button>
+      <div className="flex justify-between py-2">
+        <Button
+          onClick={() => {
+            addBeat(
+              {
+                description: description,
+                durationSeconds: parseInt(duration),
+                cameraAngle: cameraAngle,
+                id: nanoid(),
+              },
+              actId,
+              position
+            );
+            setShowForm(false);
+          }}
+        >
+          Add
+        </Button>
+        <Button
+          variant={"secondary"}
+          onClick={() => {
+            setShowForm(false);
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
     </div>
   );
 }
@@ -246,35 +270,38 @@ function BeatView({
   // todo - incorporate all of the beat info
   const [editMode, setEditMode] = React.useState(false);
   return (
-    <div className="bg-[#2d2f48] p-4 rounded-lg">
-      {editMode && (
+    <div className="bg-[#2d2f48] p-4 rounded-lg w-80">
+      {editMode ? (
         <UpdateBeatForm
           beat={beat}
           updateBeat={updateBeat}
           setEditMode={setEditMode}
         />
+      ) : (
+        <>
+          <p>{beat.description}</p>
+          <p>Camera Angle: {beat.cameraAngle}</p>
+          <p>durationSeconds: {beat.durationSeconds}</p>
+          <div className="flex justify-between">
+            <Button
+              variant={"secondary"}
+              onClick={() => {
+                setEditMode(true);
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              variant={"destructive"}
+              onClick={() => {
+                handleDelete(beat.id);
+              }}
+            >
+              Delete
+            </Button>
+          </div>
+        </>
       )}
-      <p>{beat.description}</p>
-      <p>Camera Angle: {beat.cameraAngle}</p>
-      <p>durationSeconds: {beat.durationSeconds}</p>
-      <div className="flex justify-between">
-        <Button
-          variant={"secondary"}
-          onClick={() => {
-            setEditMode(true);
-          }}
-        >
-          Edit
-        </Button>
-        <Button
-          variant={"destructive"}
-          onClick={() => {
-            handleDelete(beat.id);
-          }}
-        >
-          Delete
-        </Button>
-      </div>
     </div>
   );
 }
@@ -296,15 +323,16 @@ function UpdateBeatForm({
     <div>
       <Input
         type="text"
+        autoFocus
         placeholder="Description"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
       <Input
-        type="text"
+        type="number"
         placeholder="Duration"
         value={duration}
-        onChange={(e) => setDuration(e.target.value)}
+        onChange={(e) => setDuration(Number(e.target.value))}
       />
       <Input
         type="text"
@@ -312,19 +340,22 @@ function UpdateBeatForm({
         value={cameraAngle}
         onChange={(e) => setCameraAngle(e.target.value)}
       />
-      <Button
-        onClick={async () => {
-          await updateBeat(beat.id, {
-            description,
-            durationSeconds: duration,
-            cameraAngle,
-            id: beat.id,
-          });
-          setEditMode(false);
-        }}
-      >
-        Update
-      </Button>
+      <div className="flex justify-between">
+        <Button
+          onClick={async () => {
+            await updateBeat(beat.id, {
+              description,
+              durationSeconds: duration,
+              cameraAngle,
+              id: beat.id,
+            });
+            setEditMode(false);
+          }}
+        >
+          Update
+        </Button>
+        <Button variant={"secondary"}>Cancel</Button>
+      </div>
     </div>
   );
 }
@@ -343,7 +374,7 @@ function ActView({
   deleteBeat: any;
 }) {
   return (
-    <div>
+    <div className="overflow-auto">
       <div className="flex items-center mb-4 justify-between">
         <div className="bg-[#6f42c1] text-2xl font-bold px-4 py-1 rounded-l-full">
           {act.title}
@@ -356,7 +387,7 @@ function ActView({
         <AddBeat addBeat={addBeat} position={0} actId={act.id} />
         {act.beats.map((beat, idx) => {
           return (
-            <>
+            <div className="flex" key={beat.id}>
               <BeatView
                 key={beat.id}
                 beat={beat}
@@ -369,7 +400,7 @@ function ActView({
                 position={idx + 1}
                 actId={act.id}
               />
-            </>
+            </div>
           );
         })}
       </div>
